1f2973ab716b053fad71e8b93c660c3411239387
Jose Pagan <jm.pagan@gmail.com>
2020-08-12 13:44:39 +0100

    added config stuff

--- app.js
+++ app.js
@@ -1,5 +1,10 @@
 const express = require('express');
+require('dotenv').config();
+
 const app = express();
 const port = 3000;
-app.get('/',(req, res) => res.send("Hello World!"));
+app.get('/',(req, res) => {
+  console.log(process.env.HOST);
+  res.send("Hello World!")
+});
 app.listen(port, ()=> console.log(`App listening at ${port}`))
---
+++ simple.js
@@ -0,0 +1,9 @@
+require('dotenv').config()
+
+const hostname = process.env.HOST;
+const database = process.env.DATABASE;
+const port = process.env.PORT;
+
+console.log(hostname);
+console.log(database);
+console.log(port);
