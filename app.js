9776ebcd62db772a094656eb4ef9c956719292d4
Jose Pagan <jose@pop-os.localdomain>
2020-08-24 22:58:30 +0100

    more shit added

--- app.js
+++ app.js
@@ -3,6 +3,8 @@ const helmet = require('helmet');
 const morgan = require('morgan');
 require('dotenv').config();
 
+const tasks = ["task1", "task2", "task3"];
+
 const app = express();
 const port = process.env.PORT;
 app.set('view engine','pug');
@@ -17,7 +19,13 @@ app.get('/',(req, res) => {
   res.render('index', {
     title: "My express App",
     message: "Hello world html edition"
-  })
+  });
+
+  app.get('/api/tasks', (req, res) => {
+    res.send(tasks)
+  } )
+
+
 
 });
 app.listen(port, () => console.log(`App listening at ${port}`))
