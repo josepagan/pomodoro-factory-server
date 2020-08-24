57aa0905f22184478b181a560560cc8092062a34
Jose Pagan <jose@pop-os.localdomain>
2020-08-24 13:32:52 +0100

    middleware added

--- app.js
+++ app.js
@@ -1,10 +1,23 @@
 const express = require('express');
+const helmet = require('helmet');
+const morgan = require('morgan');
 require('dotenv').config();
 
 const app = express();
-const port = 3000;
+const port = process.env.PORT;
+app.set('view engine','pug');
+
+
+app.use(express.static('public'))
+app.use(helmet());
+app.use(morgan('tiny'));
+
 app.get('/',(req, res) => {
   console.log(process.env.HOST);
-  res.send("Hello World!")
+  res.render('index', {
+    title: "My express App",
+    message: "Hello world html edition"
+  })
+
 });
-app.listen(port, ()=> console.log(`App listening at ${port}`))
+app.listen(port, () => console.log(`App listening at ${port}`))
