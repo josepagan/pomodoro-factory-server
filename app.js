856ec8b7ae54f8c691c60705d13d55592517f6d8
Jose Pagan <jm.pagan@gmail.com>
2020-08-27 12:45:01 +0100

    fixed scope bug

--- app.js
+++ app.js
@@ -24,34 +24,38 @@ app.use(helmet());
 app.use(morgan('tiny'));
 
 app.get('/',(req, res) => {
-  console.log(process.env.HOST);
   res.render('index', {
     title: "My express App",
     message: "Hello world html edition"
   });
 
-  app.get('/api/tasks', (req, res) => {
-    res.send(tasks)
-  } )
-
-  app.post('/api/tasks', (req, res) => {
-    const schema = {
-      text:Joi.string().required()
-    }
-    const result = Joi.validate(req.body, schema);
-    console.log(result)
-
-    const newtask = {
-      //id to be assigned by database eventually
-      taskID:tasks.length + 1,
-      text:req.body.tasktext,
-      pomodoros:0,
-      completed:false
-    }
-    tasks.push(newtask);
-    res.send(newtask);
-  });
-
-
 });
+app.get('/api/tasks', (req, res) => {
+  res.send(tasks)
+} )
+
+app.post('/api/tasks', (req, res) => {
+  const schema = Joi.object({
+    text:Joi.string().required()
+  })
+
+  const result = schema.validate(req.body)
+  console.log(result)
+  if (result.error) {
+    res.status(400).send(result.error.details[0].message);
+    return
+  }
+
+  const newtask = {
+    //id to be assigned by database eventually
+    taskID:tasks.length + 1,
+    text:req.body.tasktext,
+    pomodoros:0,
+    completed:false
+  }
+  tasks.push(newtask);
+  res.send(newtask);
+});
+
+
 app.listen(port, () => console.log(`App listening at ${port}`))
